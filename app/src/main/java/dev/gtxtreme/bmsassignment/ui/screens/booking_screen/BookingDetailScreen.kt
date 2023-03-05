eab3a5e0c4c4976dd07a96d09873c5ca8eb6aab0
ForceGT <thakkargaurav409@gmail.com>
2023-03-05 20:41:29 +0530

    Make booking details screen show text in center

--- app/src/main/java/dev/gtxtreme/bmsassignment/ui/screens/booking_screen/BookingDetailScreen.kt
+++ app/src/main/java/dev/gtxtreme/bmsassignment/ui/screens/booking_screen/BookingDetailScreen.kt
@@ -1,10 +1,16 @@
 package dev.gtxtreme.bmsassignment.ui.screens.booking_screen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun BookingDetailScreen() {
 
-    Text(text = "Booking Detail Screen")
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(text = "Booking Detail Screen", modifier = Modifier.align(Alignment.Center))
+    }
 }
